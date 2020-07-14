2a3e83485745b6a7e5111053232f42a15ebc267e
Monnyka Pin <nykanano@gmail.com>
2020-07-14 14:49:40 +0700

    -Complete Staff List Test Case

--- src/main/java/Testcase/StaffList.java
+++ src/main/java/Testcase/StaffList.java
@@ -31,7 +31,149 @@ public class StaffList extends BaseClass{
         }
     }
 
+    @Test
+    public void staffList_TCSL03(){
+         //Staff's stars earned
+        for(int i=1;i<=3;i++) {
+            String stars = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[" + i + "]/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.TextView[1]");
+            Assert.assertEquals(stars, readExcelFile(11 + i, 1, "orgData"));
+            print(stars + " \n");
+        }
+    }
 
+    @Test
+    public void staffList_TCSL04(){
+        //User Info
+        for(int i=1;i<=3;i++){
+            String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout["+i+"]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[1]");
+            Assert.assertEquals(staff, readExcelFile(11+i, 0, "orgData"));
+            print(staff+" ");
+            String position = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout["+i+"]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[2]");
+            Assert.assertEquals(position, readExcelFile(11+i,2,"orgData"));
+            print(position+" ");
+            String stars = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout["+i+"]/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.TextView[1]");
+            Assert.assertEquals(stars,readExcelFile(11+i,1,"orgData"));
+            print(stars+" \n");
+        }
+    }
+
+    @Test
+    public void staffList_TCSL06(){
+        //Tap on staff name
+        String name = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[1]");
+        clickXpathElement("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.view.View");
+        Assert.assertEquals(getText("com.pathmazing.stars:id/text_view_full_name"),name);
+        driver.navigate().back();
+    }
+
+    @Test
+    public void staffList_TCSL07(){
+        //Tap on search icon
+        clickById("com.pathmazing.stars:id/menu_bar_search");
+        clickById("com.pathmazing.stars:id/btnCloseSearch");
+    }
+
+    @Test
+    public void staffList_TCSL08(){
+        //Search Staff
+        clickById("com.pathmazing.stars:id/menu_bar_search");
+        sendKeyById("com.pathmazing.stars:id/tvSearchEmployee","Leon");
+        String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[1]");
+        Assert.assertEquals(staff, readExcelFile(13, 0, "orgData"));
+        print(staff+" ");
+        String position = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[2]");
+        Assert.assertEquals(position, readExcelFile(13,2,"orgData"));
+        print(position+" ");
+        String stars = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.TextView[1]");
+        Assert.assertEquals(stars,readExcelFile(13,1,"orgData"));
+        print(stars+" \n");
+    }
+
+    @Test
+    public void staffList_TCSL09() {
+        clickById("com.pathmazing.stars:id/btnCloseSearch");
+    }
+
+    @Test
+    public void staffList_TCSL10() {
+        //Tap on heart icon
+        clickById("com.pathmazing.stars:id/menu_bar_favorite");
+        String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[1]");
+        Assert.assertEquals(staff, readExcelFile(14, 0, "orgData"));
+        print(staff+" ");
+        String position = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.TextView[2]");
+        Assert.assertEquals(position, readExcelFile(14,2,"orgData"));
+        print(position+" ");
+        String stars = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.TextView[1]");
+        Assert.assertEquals(stars,readExcelFile(14,1,"orgData"));
+        print(stars+" \n");
+        clickById("com.pathmazing.stars:id/menu_bar_favorite");
+    }
+
+    @Test
+    public void staffList_TCSL11() {
+        //Tap on grid
+        clickById("com.pathmazing.stars:id/menu_bar_change");
+        for(int i=1;i<=3;i++){
+            String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout["+i+"]/android.widget.LinearLayout[3]/android.widget.TextView");
+            Assert.assertEquals(staff, readExcelFile(11+i, 0, "orgData"));
+            print(staff+" ");
+        }
+    }
+
+    @Test
+    public void staffList_TCSL12() {
+        //Tap on filter icon
+        clickById("com.pathmazing.stars:id/menu_bar_filter");
+    }
+
+    @Test
+    public void staffList_TCSL13() {
+        //Tap on filter icon
+        //Filter by male
+        clickById("com.pathmazing.stars:id/rbDefaultMale");
+        clickById("com.pathmazing.stars:id/btn_ok");
+        for(int i=1;i<=3;i++){
+            String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout["+i+"]/android.widget.LinearLayout[3]/android.widget.TextView");
+            Assert.assertEquals(staff, readExcelFile(11+i, 0, "orgData"));
+            print(staff+" \n");
+        }
+
+        //Filter by female
+        clickById("com.pathmazing.stars:id/menu_bar_filter");
+        clickById("com.pathmazing.stars:id/rbDefaultFemale");
+        clickById("com.pathmazing.stars:id/btn_ok");
+        Assert.assertEquals(getText("com.pathmazing.stars:id/text_msg"), "No staff found");
+
+        //Filter Project Manager
+        clickById("com.pathmazing.stars:id/menu_bar_filter");
+        clickById("com.pathmazing.stars:id/rbAll");
+        clickXpathElement("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[3]/android.widget.CheckBox");
+        clickById("com.pathmazing.stars:id/btn_ok");
+        for(int i=1;i<=1;i++){
+            String staff = getTextByXpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout["+i+"]/android.widget.LinearLayout[3]/android.widget.TextView");
+            Assert.assertEquals(staff, readExcelFile(5+i, 1, "staffList"));
+            print(staff+" ");
+        }
+
+        //Resetting Filter
+        clickById("com.pathmazing.stars:id/menu_bar_filter");
+        clickById("com.pathmazing.stars:id/image_view_filter");
+        clickById("com.pathmazing.stars:id/btn_ok");
+    }
+
+    @Test
+    public void staffList_TCSL14(){
+        driver.navigate().back();
+        logout();
+    }
 
 
 }
+
+
+
+
+
+
+
---
+++ src/main/java/Testcase/TeamList.java
@@ -0,0 +1,27 @@
+package Testcase;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+
+public class TeamList extends BaseClass {
+
+    @Test
+    public void teamList_TCTL00() throws InterruptedException {
+        loginStaff();
+        Thread.sleep(4000);
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.tap(PointOption.point(525, 281)).perform();
+
+        Thread.sleep(4000);
+        TouchAction swipe = new TouchAction(driver);
+        swipe.press(PointOption.point(972,570))
+                .waitAction(waitOptions(Duration.ofMillis(800)))
+                .moveTo(PointOption.point(10,570))
+                .release()
+                .perform();
+    }
+}
